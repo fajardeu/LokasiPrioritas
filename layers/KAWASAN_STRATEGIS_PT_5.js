a8a50452300bfac0e2f9027481b32efead92e033
fajardeu <163820519+fajardeu@users.noreply.github.com>
2024-03-18 13:57:06 +0700

    Add files via upload

---
+++ layers/KAWASAN_STRATEGIS_PT_5.js
@@ -0,0 +1,1 @@
+var json_KAWASAN_STRATEGIS_PT_5 = {"type":"FeatureCollection","name":"KAWASAN_STRATEGIS_PT_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
